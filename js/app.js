440bcf3ef7e7a19a81c1bfa8c64c5f370c792742
Sephula Mokeona <sephulam@tihsa.co.za>
2020-06-30 14:15:40 +0200

    added create and setting of comments on the dom

--- js/app.js
+++ js/app.js
@@ -46,15 +46,33 @@ const trash = () => {
   return span;
 };
 
+const createComment = () => {
+  let comment, bin, text, image;
+
+  comment = elemFactory("div");
+  image = cardImage()
+  text = commentText(`ddddddddddddddddddddddddddddddddddd`)
+  bin = trash()
+
+  comment.classList.add("comment")
+  comment.appendChild(image);
+  comment.appendChild(text);
+  comment.appendChild(bin);
+
+  return comment;
+};
+
 const setComment = () => {
   const viewComments = document.getElementById("view-comments");
+  const comment = createComment();
+  console.log(comment);
+
+  viewComments.appendChild(comment);
 };
 
 const inputData = (num) => {
-  let text = document.getElementById("num");
+  let text = document.getElementById(num).value;
   if(!text) throw "Comment cannot be empty";
+  console.log(text)
   return text;
 };
-
-
-
