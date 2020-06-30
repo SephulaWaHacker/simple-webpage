4e2f733d7b4cd135ecbd13346d92bc523bfa8034
Sb <sb@example.com>
2020-06-30 21:10:43 +0200

    saving comments to db

--- js/app.js
+++ js/app.js
@@ -76,3 +76,65 @@ const inputData = (num) => {
   console.log(text)
   return text;
 };
+
+if (!window.indexedDB) {
+  throw new Error("Your browser doesn't support IndexedDB.");
+};
+
+let db;
+const request = window.indexedDB.open("Posts", 1);
+
+request.onupgradeneeded = (e) => {
+  let db = e.target.result;
+  let store = db.createObjectStore("commentsStore", {
+        keyPath: "id",
+        autoIncrement: true
+  });
+};
+
+request.onerror = (e) => {
+  console.log(`Error: ${e.target.errorCode}`);
+};
+
+request.onsuccess = (e) => {
+  db = request.result;
+};
+
+request.onupgradeneeded = (e) => {
+  let db = e.target.result;
+  let store = db.createObjectStore("commentsStore", {
+    keyPath: "id",
+    autoIncrement: true
+  });
+  
+};
+
+const saveComment = (postId) => {
+  let data, request, getAll, store, lastValue; 
+  
+  data = {
+    postId: postId,
+    comment: inputData(postId)
+  };
+  
+  request = db.transaction(["commentsStore"], "readwrite");
+  store = request.objectStore("commentsStore");
+  store.add(data);
+  
+  getAll = store.getAll()
+  
+  request.onsuccess = (e) => {
+    console.log(`Success: ${e.target.result}`);
+  };
+  
+  request.onerror = (e) => {
+    throw new Error(`Error: ${e.target.errorCode}`);
+  }
+  
+  request.oncomplete = (e) => {
+    lastValue = getAll.result[getAll.result.length - 1]
+    console.log(getAll.result)
+    console.log(lastValue)
+    // setComment()  
+ }
+};
